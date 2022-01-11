40e44b89d717c93fce61519f2b9216db2838b5a1
thedoft <thedoft@gmail.com>
2022-01-11 20:28:17 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <el-main>
+    <span v-if="error">{{ error }}</span>
+    <el-table v-else :data="data" border fit style="width: 100%">
+      <el-table-column prop="ID" label="Артикул" />
+      <el-table-column prop="NAME" label="Наименование" min-width="300px" />
+      <el-table-column prop="QUANTITY" label="Общий остаток" />
+      <el-table-column prop="FREE_QUANTITY" label="Свободный остаток" />
+      <el-table-column prop="RESERVED_QUANTITY" label="В резерве" />
+      <el-table-column prop="INCOME_QUANTITY" label="Ожидаемый приход" />
+      <el-table-column prop="DATE_INCOME" label="Ожидаемая дата прихода" />
+      <el-table-column prop="PRICE_FOR_PARTNER" label="Цена для партнеров" />
+      <el-table-column prop="PRICE_MRC" label="Цена розничная" />
+    </el-table>
+  </el-main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      data: null,
+      error: null,
+    };
+  },
+  async mounted() {
+    try {
+      const { data } = await axios.get('https://kub-xml-parse.herokuapp.com/');
+      console.log(data);
+    } catch {
+      this.error = 'Не удалось загрузить информацию';
+    }
+  },
 }
 </script>
 
@@ -21,6 +45,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 0;
+}
+
+.el-table .cell {
+  word-break: normal !important;
 }
 </style>
